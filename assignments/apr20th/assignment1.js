d4a302d1cecc84f76ff467bfd2fd6a4ff23c8081
Hritvik Mohan <hritvik5005@gmail.com>
2022-05-17 16:54:54 +0530

    Added assignments folders.

---
+++ assignments/apr20th/assignment1.js
@@ -0,0 +1,6 @@
+function astroGuru (numberOfChildren, spouseName, location, job){
+    let ans = "You will be a "+ job +" in "+location+", and married to "+ spouseName+ " with "+ numberOfChildren +" kids."
+    return console.log(ans);
+  }
+  
+  astroGuru(2, "Hritvika", "India", "Dev");

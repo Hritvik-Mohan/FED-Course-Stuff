5696e633c288634d977ae47594e3841938163896
Hritvik Mohan <hritvik5005@gmail.com>
2022-05-24 15:50:44 +0530

    Added Advance Construct - Array Questions from JS Video Library

---
+++ assignments/JavaScript Video Library/Advance Construct - Array/question2.js
@@ -0,0 +1,80 @@
+// approach 1
+let mean1 = (nums) => {
+
+    let sum = 0;
+    for( i=0; i<nums.length; i++ ){
+        sum += nums[i];
+    }
+
+    console.log("Mean using first approach: "+sum/nums.length);
+
+}
+
+// using rest parameters
+const mean = (...nums) => nums.reduce((acc, val) => acc+val, 0)/nums.length
+
+const nums1 = [ 3, 5, 4, 4, 1, 1, 2, 3 ];
+const nums2 = [ 1, 2, 3 ];
+
+mean1(nums1);
+mean1(nums2);
+
+console.log("Mean using second apporach: " + mean(...nums1));
+console.log("Mean using second apporach: " + mean(...nums2));
+
+console.log("Mean using second apporach: " + mean(...[ 3, 5, 4, 4, 1, 1, 2, 3 ]));
+console.log("Mean using second apporach: " + mean(...[ 1, 2, 3 ]));
+
+
+let median = (nums) => {
+    let m = 0;
+    let n = nums.length;
+    nums.sort((a,b)=>a-b); //sorting the array in the accending order
+
+    if(n%2 == 0){ //even
+
+        let a = nums[(n/2)-1];
+        let b = nums[(n/2)+1];
+
+        console.log(a, b);
+
+        m = (nums[(n/2)-1] + nums[(n/2)+1])/2;
+    } else {
+        // m = nums[n/2]; // 1.5 is not a valid index
+        m = nums[parseInt(n/2)];
+    }
+    return m;
+}
+
+console.log("Median using first apporach: " + median(nums1));
+console.log("Median using first apporach: " + median(nums2));
+
+let hm = new Map();
+
+let  mode = (nums) => {
+
+    for(let i=0; i<nums.length; i++){
+        if(hm.has(nums[i]) == false){ //
+            hm.set(nums[i], 1);
+        } else {
+            let count = hm.get(nums[i]);
+            count++;
+            hm.set(nums[i], count);
+        }
+    }
+    let modArray = [];
+    for(let [key, value] of hm){
+        if(value>1){
+            modArray.push(key);
+        }
+    }
+
+    return modArray;
+}
+
+let nums3 = [ 3, 5, 4, 4, 1, 1, 2, 3, 3, 5, 4, 4, 1, 1, 2, 3, 4, 4, 1, 1, 2, 3, 3, 5];
+
+console.log("Mode is " + mode(nums3));
+
+
+console.table(hm);
---
+++ assignments/JavaScript Video Library/Advance Construct - Array/questions1.js
@@ -0,0 +1,121 @@
+const arr = [1,1,2,2,2,3,3,6,6,3,3,4,4,4,4,4,9]
+
+// key -> numbers
+// pair -> count of those numbers
+
+let hm = new Map();
+
+for(let i=0; i<arr.length; i++){
+
+    if(hm.has(arr[i]) == false){ // if value is not there in the hashmap
+
+        hm.set(arr[i], 1); // insert the value in the hashmap
+
+    } else { // update the count
+
+        let count = hm.get(arr[i]);
+        count++;
+        hm.set(arr[i], count);
+
+    }
+
+}
+
+console.table(hm);
+console.log(hm);
+
+for(let [key, value] of hm){
+    if(value !== 1){
+        console.log(`${key} is occuring ${value} times.`);
+    } else {
+        console.log(`${key} is occuring ${value} time.`);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function mean(numbers) {
+//     var total = 0, i;
+//     for (i = 0; i < numbers.length; i += 1) {
+//         total += numbers[i];
+//     }
+//     return total / numbers.length;
+// }
+ 
+
+// function median(numbers) {
+//     var median = 0, numsLen = numbers.length;
+//     numbers.sort();
+ 
+//     if (
+//         numsLen % 2 === 0 // is even
+//     ) {
+//         // average of two middle numbers
+//         median = (numbers[numsLen / 2 - 1] + numbers[numsLen / 2]) / 2;
+//     } else { // is odd
+//         // middle number only
+//         median = numbers[(numsLen - 1) / 2];
+//     }
+ 
+//     return median;
+// }
+ 
+
+// function mode(numbers) {
+//     // as result can be bimodal or multi-modal,
+//     // the returned result is provided as an array
+//     // mode of [3, 5, 4, 4, 1, 1, 2, 3] = [1, 3, 4]
+//     var modes = [], count = [], i, number, maxIndex = 0;
+ 
+//     for (i = 0; i < numbers.length; i += 1) {
+//         number = numbers[i];
+//         count[number] = (count[number] || 0) + 1;
+//         if (count[number] > maxIndex) {
+//             maxIndex = count[number];
+//         }
+//     }
+ 
+//     for (i in count)
+//         if (count.hasOwnProperty(i)) {
+//             if (count[i] === maxIndex) {
+//                 modes.push(Number(i));
+//             }
+//         }
+ 
+//     return modes;
+// }
+
+// console.log(mean([1,2,3]))
+// console.log(median([3, 5, 4, 4, 1, 1, 2, 3]))
+// console.log(mode([3, 5, 4, 4, 1, 1, 2, 3]))

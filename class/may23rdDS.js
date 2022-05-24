bcb095d4ce13933c89c3f9ad1a12921dc57e5616
Hritvik Mohan <hritvik5005@gmail.com>
2022-05-24 15:54:43 +0530

    Added may23rd doubt session code examples

---
+++ class/may23rdDS.js
@@ -0,0 +1,68 @@
+// TA2 Abhishek Gard
+
+
+// Clousers
+
+
+// setTime Out
+
+// for(var i=0; i<4; i++){
+//     setTimeout(()=>{console.log(i)}, i*1000);
+// }
+// /*
+// 4
+// 4
+// 4
+// 4
+// */
+
+// for(let i=0; i<4; i++){
+//     setTimeout(()=>{console.log(i)}, i*1000);
+// }
+// /*
+// 0
+// 1
+// 2
+// 3
+// */
+
+
+
+
+// /*
+// 4
+// 0
+// 4
+// 1
+// 4
+// 2
+// 4
+// 3
+// */
+
+// {let i=0;} // lexical scope
+// {if(i<4)}
+// i++;
+
+
+
+// function sayHi(){
+//     console.log(name);
+//     console.log(age);
+//     var name = 'abhi';
+//     let age=21;
+// }
+
+// console.log(sayHi())
+
+
+// const a={
+//     name:'abhi'
+// }
+// const b={
+//     name:'abhi'
+// }
+
+// console.log(a==b);
+
+// console.log(a.name==b.name);
